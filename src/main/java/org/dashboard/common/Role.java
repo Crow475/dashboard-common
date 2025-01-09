dbb1ae9b88276209ca9d6b0769a27da861d6de37
Crow475 <116875872+Crow475@users.noreply.github.com>
2025-01-09 20:40:44 +0400

    Add Role adn UserOfDashboard

---
+++ src/main/java/org/dashboard/common/Role.java
@@ -0,0 +1,9 @@
+package org.dashboard.common;
+
+public enum Role {
+    OWNER,
+    ADMIN,
+    EDITOR,
+    VIEWER,
+    NONE
+}
---
+++ src/main/java/org/dashboard/common/models/UserOfDashboard.java
@@ -0,0 +1,39 @@
+package org.dashboard.common.models;
+
+import org.dashboard.common.Role;
+
+public class UserOfDashboard {
+    private String username;
+    private String dashboardName;
+    private Role role;
+
+    public UserOfDashboard(String username, String dashboardName, Role role) {
+        this.username = username;
+        this.dashboardName = dashboardName;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getDashboardName() {
+        return dashboardName;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public void setDashboardName(String dashboardName) {
+        this.dashboardName = dashboardName;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+}
